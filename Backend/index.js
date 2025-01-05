a2c265a540b3327d1d2375f4e95ceaa4fc980658
unknown <vaibhavagarwaleverest@gmail.com>
2025-01-06 00:56:56 +0530

    Changes made by Vaibhav

--- Backend/index.js
+++ Backend/index.js
@@ -1,4 +1,3 @@
-
 import asyncHandlers from "./asyncHandlers.js";
 import express from "express";
 import cookieParser from "cookie-parser";
@@ -6,12 +5,8 @@ import { Router } from "express";
 import cors from "cors"
 import {EventSource} from "eventsource"
 // import ApiResponse from "./ApiResponse.js";
-<<<<<<< HEAD
 import ApiResponse from "./ApiResponse.js";
 
-=======
-// import ApiResponse from "./ApiResponse.js";
->>>>>>> 9c4e51abe70bc7bb9f502240b820f45fd6bfc8fe
 
 import dotenv from "dotenv";
 
